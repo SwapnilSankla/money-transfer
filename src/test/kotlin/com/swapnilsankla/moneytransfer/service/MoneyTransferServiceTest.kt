92d319f04629f4ab56601aa4a3a49422726b1099
Swapnil Sankla <swapnil.sankla@gmail.com>
2019-08-03 15:25:51 +0530

    Add tests to make sure the debit and credit are done properly in money transfer service

--- src/test/kotlin/com/swapnilsankla/moneytransfer/service/MoneyTransferServiceTest.kt
+++ src/test/kotlin/com/swapnilsankla/moneytransfer/service/MoneyTransferServiceTest.kt
@@ -19,6 +19,30 @@ class MoneyTransferServiceTest {
     }
 
     @Test
+    fun `on successful transfer the amount should be debited from sender's account`() {
+        val moneyTransferService = MoneyTransferService()
+        val from = Account("1234", 10000.0)
+        moneyTransferService.transfer(
+                from = from,
+                to = Account("4321", 5000.0),
+                amount = 100.0
+        )
+        Assertions.assertEquals(9900.0, from.balance)
+    }
+
+    @Test
+    fun `on successful transfer the amount should be credit to receiver's account`() {
+        val moneyTransferService = MoneyTransferService()
+        val to = Account("4321", 5000.0)
+        moneyTransferService.transfer(
+                from = Account("1234", 10000.0),
+                to = to,
+                amount = 100.0
+        )
+        Assertions.assertEquals(5100.0, to.balance)
+    }
+
+    @Test
     fun `should be able to transfer money from 1 account to another account only if account has sufficient balance`() {
         val moneyTransferService = MoneyTransferService()
 
@@ -29,5 +53,29 @@ class MoneyTransferServiceTest {
         )
 
         Assertions.assertFalse(transferred)
+    }
+
+    @Test
+    fun `on unsuccessful transfer the amount should not be debited from sender's account`() {
+        val moneyTransferService = MoneyTransferService()
+        val from = Account("1234", 10000.0)
+        moneyTransferService.transfer(
+                from = from,
+                to = Account("4321", 5000.0),
+                amount = 11100.0
+        )
+        Assertions.assertEquals(10000.0, from.balance)
+    }
+
+    @Test
+    fun `on unsuccessful transfer the amount should be credit to receiver's account`() {
+        val moneyTransferService = MoneyTransferService()
+        val to = Account("4321", 5000.0)
+        moneyTransferService.transfer(
+                from = Account("1234", 10000.0),
+                to = to,
+                amount = 11100.0
+        )
+        Assertions.assertEquals(5000.0, to.balance)
     }
 }

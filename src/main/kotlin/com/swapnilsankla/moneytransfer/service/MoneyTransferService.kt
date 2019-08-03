87246b624b9c5b930d8400d868e8d45ee935b07d
Swapnil Sankla <swapnil.sankla@gmail.com>
2019-08-03 15:09:50 +0530

    Add guard in money transfer to prevent transfer on insufficient balance

--- src/main/kotlin/com/swapnilsankla/moneytransfer/service/MoneyTransferService.kt
+++ src/main/kotlin/com/swapnilsankla/moneytransfer/service/MoneyTransferService.kt
@@ -4,9 +4,12 @@ import com.swapnilsankla.moneytransfer.model.Account
 
 class MoneyTransferService {
     fun transfer(from: Account, to: Account, amount: Double): Boolean {
-        from.debit(amount)
-        to.credit(amount)
-        return true
+        if (from.balance >= amount) {
+            from.debit(amount)
+            to.credit(amount)
+            return true
+        }
+        return false
     }
 }
 
--- src/test/kotlin/com/swapnilsankla/moneytransfer/service/MoneyTransferServiceTest.kt
+++ src/test/kotlin/com/swapnilsankla/moneytransfer/service/MoneyTransferServiceTest.kt
@@ -9,8 +9,17 @@ class MoneyTransferServiceTest {
     fun `should be able to transfer money from 1 account to another account`() {
         val moneyTransferService = MoneyTransferService()
 
-        val transferred = moneyTransferService.transfer(Account("1234",10000.0), Account("4321", 5000.0), 100.0)
+        val transferred = moneyTransferService.transfer(Account("1234", 10000.0), Account("4321", 5000.0), 100.0)
 
         Assertions.assertTrue(transferred)
+    }
+
+    @Test
+    fun `should be able to transfer money from 1 account to another account only if account has sufficient balance`() {
+        val moneyTransferService = MoneyTransferService()
+
+        val transferred = moneyTransferService.transfer(Account("1234", 1000.0), Account("4321", 5000.0), 2000.0)
+
+        Assertions.assertFalse(transferred)
     }
 }

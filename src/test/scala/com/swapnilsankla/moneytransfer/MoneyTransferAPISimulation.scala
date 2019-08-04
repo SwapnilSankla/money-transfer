cab032fa3bf428ee5c957c9bc5a472e8caa4f712
Swapnil Sankla <swapnil.sankla@gmail.com>
2019-08-04 16:10:56 +0530

    Gatling performance test for transaction API

---
+++ src/test/scala/com/swapnilsankla/moneytransfer/MoneyTransferAPISimulation.scala
@@ -0,0 +1,23 @@
+package com.swapnilsankla.moneytransfer
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class MoneyTransferAPISimulation extends Simulation {
+  val httpConf = http.baseUrl("http://localhost:8080")
+    .acceptHeader("application/json")
+
+  val scn = scenario("Basic Simulation")
+    .exec(http("request_1")
+      .post("/transaction")
+      .body(StringBody(
+      """{
+          "fromAccountNumber": "1234",
+          "toAccountNumber": "4321",
+          "amount": 1.0
+       }"""))
+      .asJson
+      .check(status.is(200)))
+
+  setUp(scn.inject(atOnceUsers(1000))).protocols(httpConf)
+}
